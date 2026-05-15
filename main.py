024a7b7c000d9c3dad8ee02f090c81396b810c15
Hadi493 <hadialam493@gmail.com>
2026-05-15 11:37:09 +0600

    feat: forwarding from multiple channels, Unoptimized implementation

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ from dotenv import load_dotenv
 from telethon import utils
 from telethon.sync import TelegramClient, events
 from telethon.tl.functions.messages import GetHistoryRequest
+import asyncio
 
 load_dotenv()
 
 api_id = os.getenv('API_ID')
-import asyncio
 api_hash = os.getenv('API_HASH')
+
+sleep_time = 5
 
 client = TelegramClient('cyber-green', api_id, api_hash)
 
-async def get_last_msg(channel_name):
+channels = ["@CyberGreenNews", "@SHADOWMAN997", "@SorwarAlam"]
+
+
+async def get_last_msg(channels):
     try:
-        entity = await client.get_entity(channel_name)
-        async for msg in client.iter_messages(entity, limit=1):
-            return msg
+        msgs = []
+        for channel in channels:
+            async for msg in client.iter_messages(channel, limit=1):
+                msgs.append(msg)
+                print(msgs)
+        return msgs
+
     except Exception as e:
         print(f"Error: {e}")
         return None
@@ -25,53 +34,43 @@ async def get_last_msg(channel_name):
 async def forward_msg(msg, dest):
     try:
         if not msg:
+            print(msg)
             return None
 
         dest_entity = await client.get_entity(dest)
-
         forwarded = await client.forward_messages(
             entity = dest_entity,
             messages = msg.id,
             from_peer = msg.chat_id
         )
-
         print(f"Msg forwarded from: {msg.id}")
         return forwarded
-
     except Exception as e:
         print(f"Error: {e}")
         return None
 
-
-
 async def main():
     print("Starting....")
-    current_msg_id = None
 
+    current_msg_ids = []
     while True:
         try:
-            new_msg = await get_last_msg({"@CyberGreenNews")
-
-            if new_msg:
-                print(f"[{datetime.datetime.now()}] Latest msg ID: {new_msg.id}")
-
-                if new_msg.id != current_msg_id:
-                    print(f"New msg detected")
-                    print(f"   Text: {new_msg.text[:80] if new_msg.text else 'No text'}...")
-
-                    await forward_msg(new_msg, 'me')
-
-                    current_msg_id = new_msg.id
-
-                else:
-                    print("No new msg detected")
-
-            await asyncio.sleep(5)
-
+            new_msgs = await get_last_msg(channels)
+            for new_msg in new_msgs:
+                print(new_msg)
+                if new_msg:
+                    print(f"[{datetime.datetime.now()}] Latest msg ID: {new_msg.id}")
+                    if new_msg.id not in current_msg_ids:
+                        print(f"New msg detected")
+                        print(f"   Text: {new_msg.text[:80] if new_msg.text else 'No text'}...")
+                        await forward_msg(new_msg, 'me')
+                        current_msg_ids.append(new_msg.id)
+                    else:
+                        print("No new msg detected")
+                await asyncio.sleep(sleep_time)
         except Exception as e:
             print(f"Error: {e}")
             return None
-
 
 if __name__ == '__main__':
     with client:
